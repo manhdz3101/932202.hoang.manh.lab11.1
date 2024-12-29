5de1c9024c4f35111d9990d71c3578c502af2c12
manhdz3101 <163209199+manhdz3101@users.noreply.github.com>
2024-12-29 15:01:21 +0700

    Add files via upload

---
+++ Service/Services.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication1.Services
+{
+    public class CalcService
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+        public CalcService()
+        {
+            var random = new Random();
+            Num1 = random.Next(0, 11);
+            Num2 = random.Next(0, 11);
+        }
+
+        public int Add() => Num1 + Num2;
+        public int Sub() => Num1 - Num2;
+        public int Mul() => Num1 * Num2;
+        public int? Div() => Num2 != 0 ? Num1 / Num2 : null;
+    }
+}
